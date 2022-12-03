0b274a67d084527527123b79ded4ba2cc0744a39
Jan Philipp <knallisworld@googlemail.com>
2022-12-04 00:16:47 +0100

    Improve using intersection, better multi_intersection

--- src/aoc2022/day03/part1.rs
+++ src/aoc2022/day03/part1.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 use crate::aoc2022::lib::common::{measure_time_and_print, PuzzleScope};
 use crate::aoc2022::lib::io;
@@ -18,57 +18,20 @@ fn resolve_priority_sum(scope: &PuzzleScope, puzzle: &str) -> i32 {
     let mut sum = 0;
     for line in io::read_puzzle_as_list(scope.day(), puzzle) {
         let compartment_size = line.len() / 2;
-        let compartment1: Vec<char> = line.chars()
+        let compartment1: HashSet<_> = line.chars()
             .take(compartment_size)
             .collect();
-        let compartment2: Vec<char> = line.chars()
+        let compartment2: HashSet<_> = line.chars()
             .skip(compartment_size)
             .collect();
-        let overlaps = search_overlaps2(compartment1, compartment2);
+        let overlaps: Vec<_> = compartment1.intersection(&compartment2)
+            .collect();
         assert_eq!(1, overlaps.len());
-        let priority = decode_item(overlaps[0]);
+        let ch = **overlaps.last().unwrap();
+        let priority = decode_item(ch);
         sum += priority;
     }
     sum
-}
-
-fn count_chars(list: Vec<char>) -> HashMap<char, i32> {
-    let mut counter: HashMap<char, i32> = HashMap::new();
-    for ch in list {
-        counter.entry(ch)
-            .and_modify(|v| *v += 1)
-            .or_insert(1);
-    }
-    counter
-}
-
-pub fn search_overlaps2(list1: Vec<char>, list2: Vec<char>) -> Vec<char> {
-    let mut lists: Vec<Vec<char>> = Vec::new();
-    lists.push(list1);
-    lists.push(list2);
-    return search_overlaps(lists);
-}
-
-pub fn search_overlaps(lists: Vec<Vec<char>>) -> Vec<char> {
-    let required_size = lists.len() as i32;
-    let mut counter: HashMap<char, i32> = HashMap::new();
-    for list in lists {
-        // count chars in this list, but treat as 1
-        for (ch, count) in count_chars(list) {
-            if count > 0 {
-                counter.entry(ch)
-                    .and_modify(|v| *v += 1)
-                    .or_insert(1);
-            }
-        }
-    }
-    let mut result: Vec<char> = Vec::new();
-    for (ch, count) in counter {
-        if count >= required_size {
-            result.push(ch);
-        }
-    }
-    result
 }
 
 pub fn decode_item(ch: char) -> i32 {
--- src/aoc2022/day03/part2.rs
+++ src/aoc2022/day03/part2.rs
@@ -1,4 +1,6 @@
-use crate::aoc2022::day03::part1::{decode_item, search_overlaps};
+use std::collections::HashSet;
+
+use crate::aoc2022::day03::part1::decode_item;
 use crate::aoc2022::lib::common::{measure_time_and_print, PuzzleScope};
 use crate::aoc2022::lib::io;
 use crate::aoc2022::lib::style::{write_header, write_solution};
@@ -21,19 +23,35 @@ fn resolve_priority_sum(scope: &PuzzleScope, puzzle: &str) -> i32 {
     for g_offset in 0..(lines.len() / 3) {
         let mut group: Vec<String> = Vec::new();
         for i_offset in 0..3 {
-            group.push(lines.get(3*g_offset + i_offset).unwrap().to_string());
+            group.push(lines.get(3 * g_offset + i_offset).unwrap().to_string());
         }
         groups.push(group);
     }
 
     for group in groups {
-        let list: Vec<Vec<char>> = group.iter()
+        let list: Vec<HashSet<char>> = group.iter()
             .map(|g| g.chars().collect())
             .collect();
-        let overlaps = search_overlaps(list);
+        let overlaps = multi_intersection(&list);
         assert_eq!(1, overlaps.len());
-        let priority = decode_item(overlaps[0]);
+        let ch: char = overlaps.iter().last().copied().unwrap();
+        let priority = decode_item(ch);
         sum += priority;
     }
     sum
+}
+
+pub fn multi_intersection(collections: &Vec<HashSet<char>>) -> HashSet<char> {
+    if collections.is_empty() {
+        return HashSet::new();
+    }
+    let mut iter = collections.iter();
+    // starting with the first, this builds an intersection with all other
+    // result is the intersection of all
+    let intersection = iter.next().map(|col| {
+        iter.fold(col.iter().copied().collect::<HashSet<char>>(), |a, b| {
+            a.intersection(b).copied().collect()
+        })
+    }).unwrap();
+    intersection
 }
